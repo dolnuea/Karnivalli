95f68b55901a5138cd3e6843d0a6538b92b46a0b
tbanerjee <tbanerjee@albany.edu>
2021-09-08 15:04:50 -0400

    Socket connection setup : send and receive

--- karnivali/karnivali/asgi.py
+++ karnivali/karnivali/asgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_applicationfrom channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.urls import path
+from .consumers import GameRoom
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karnivali.settings')
 
 application = get_asgi_application()
+
+
+application= ProtocolTypeRouter(
+    {
+        'websocket':AuthMiddlewareStack(URLRouter([
+            path('ws/game/<room_code>' , GameRoom)
+
+        ]))
+    }
+)
