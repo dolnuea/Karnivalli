b62187e323071c806eaa168cfe4183df807a51ed
Jacob Clouse <jclouse@albany.edu>
2021-09-11 18:52:15 -0400

    Added on hover effect for text

--- karnivali_UI/karnivaliui/src/StartOrJoinGame/StartOrJoinGame.js
+++ karnivali_UI/karnivaliui/src/StartOrJoinGame/StartOrJoinGame.js
@@ -3,14 +3,20 @@ import reactDom from "react-dom";
 import './StartOrJoinGame.css';
 
 function SelectBox(props) {
+
+    const redAlert = () => {
+        alert("Let's party!")
+    };
+
+
+
     return (
-      <div className="Gamebox">
-        <h1>
-            {props.name}
-        </h1>
-      </div>
+        <div className="Gamebox" onClick={redAlert}>
+            <h1>
+                {props.name}
+            </h1>
+        </div>
     );
-  }
-  
-  export default SelectBox;
-  
+}
+
+export default SelectBox;
