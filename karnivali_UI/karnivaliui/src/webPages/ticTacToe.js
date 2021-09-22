ff243000540cec2b51c2f4f4b1133a9ad940e915
Luna Dagci <ldagci@albany.edu>
2021-09-22 15:54:10 -0400

    Added Background Image to Tic-Tac-Toe

--- karnivali_UI/karnivaliui/src/webPages/ticTacToe.js
+++ karnivali_UI/karnivaliui/src/webPages/ticTacToe.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './components/header';
 import TicTacToeBody from './components/ticTacToeBody';
+import ScreenImg from  '../images/wrinkledpaper.jpg';
 
 const TicTacToe = (props) => {
     return (
-        <div className="TicTacToe">
+        <div className="TicTacToe" style = {{backgroundImage : `url(${ScreenImg})` }}>
             <Header />
             <TicTacToeBody roomNumber={props.roomNumber} playColor={props.playColor} />
         </div>
