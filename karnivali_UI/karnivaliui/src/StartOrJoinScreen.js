412659586e393d0265e2bfbe3b8e6fd64588e947
Habib Affinnih <48428026+haffinnih@users.noreply.github.com>
2021-09-23 13:37:34 -0400

    Revert "Merge branch 'LunaBranch' into 'main'"
    This reverts commit 5d862360ec72dd24c1e55a09a4498834cea641e4, reversing
    changes made to 893bd0a2afce2793378d806d90e54a56f813aff7.

--- karnivali_UI/karnivaliui/src/StartOrJoinScreen.js
+++ karnivali_UI/karnivaliui/src/StartOrJoinScreen.js
@@ -14,48 +14,96 @@ function StartOrJoinScreen(props) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+    const [roomCode, setRoomCode] = useState("");
+
   // The selected game passed from GameSelectionScreen
   const selectedGame = props.location.state;
 
+  const redAlert = () => {
+    alert("Let's party!");
+  };
 
   const history = useHistory();
 
-  const routeChange = () => {
+  const routeChangeStart = () => {
+      if (!roomCode) {
+            alert("Please add a room number");
+            return;
+        }
+      console.log("startGame", roomCode);
     let path = selectedGame;
-    history.push(path);
-  };
+      
+      history.push({
+          pathname: path,
+          state: {
+              roomCode: roomCode,
+              player: "p1"
+          }
+      });
+    };
 
+    const routeChangeJoin = () => {
+        if (!roomCode) {
+            alert("Please add a room number");
+            return;
+        }
+        console.log("joinRoom", roomCode);
+        let path = selectedGame;
+        
+        history.push({
+            pathname: path,
+            state: {
+                roomCode: roomCode,
+                player: "p2"
+            }
+        });
+    };
+
+
+  //   const routeChangetoTTT = () =>{ 
+  //     let path = 'tic-tac-toe'; 
+  //     history.push(path);
+  // }
+  //   const routeChangetoRPS = () =>{ 
+  //       let path = 'rock-paper-scissor'; 
+  //       history.push(path);
+  //   }
 
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Hold up...</Modal.Title>
+          <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Please enter room number</Modal.Body>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
         <Modal.Footer>
-        <form>
-              <input type="text" />
-            <Button variant="primary" onClick={routeChange}>
-            Enter
-          </Button>
-          </form>
           <Button variant="secondary" onClick={handleClose}>
-            Cancel
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
-
+    
       <StartJoinScreen>
 
-        <h1>Selected Game: {selectedGame}</h1>
+              <label>Room Number</label>
+              <input
+                  type="text"
+                  placeholder="Type Room Number "
+                  value={roomCode}
+                  onChange={(e) => setRoomCode(e.target.value)}
+              />
 
-        <GameboxStart onClick={handleShow}>
-          <h1>Start New Game</h1>
+        <GameboxStart onClick={routeChangeStart}>
+                  <h1>Start New Game</h1>
+                  
         </GameboxStart>
 
-        <GameboxJoin onClick={handleShow}>
+              <GameboxJoin onClick={routeChangeJoin}>
           <h1>Join Game</h1>
+               
         </GameboxJoin>
       </StartJoinScreen>
     </>
