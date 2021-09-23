b6031f4d2287332c62cca44c4cc5e361074ffe09
Luna Dagci <ldagci@albany.edu>
2021-09-23 14:38:26 -0400

    Fixed Modal

--- karnivali_UI/karnivaliui/src/StartOrJoinScreen.js
+++ karnivali_UI/karnivaliui/src/StartOrJoinScreen.js
@@ -8,11 +8,13 @@ import {
 } from "./Components/StartJoin.styles";
 import { Modal, Button } from "react-bootstrap";
 
-function StartOrJoinScreen(props) {
+function StartOrJoinScreen(props) {""
   const [show, setShow] = useState(false);
-
+  const [option, setOption] = useState("");
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [roomCode, setRoomCode] = useState("");
 
   // The selected game passed from GameSelectionScreen
   const selectedGame = props.location.state;
@@ -20,11 +22,49 @@ function StartOrJoinScreen(props) {
 
   const history = useHistory();
 
-  const routeChange = () => {
+  const routeChangeStart = () => {
+    if (!roomCode) {
+          alert("Please add a room number");
+          return;
+      }
+    console.log("startGame", roomCode);
     let path = selectedGame;
     history.push(path);
-  };
+    history.push({
+      pathname: path,
+      state: {
+          roomCode: roomCode,
+          player: "p1"
+      }
+  });
+};
 
+const routeChangeJoin = () => {
+  if (!roomCode) {
+      alert("Please add a room number");
+      return;
+  }
+  console.log("joinRoom", roomCode);
+  let path = selectedGame;
+  
+  history.push({
+      pathname: path,
+      state: {
+          roomCode: roomCode,
+          player: "p2"
+      }
+  });
+};
+
+const routeChange = () => {
+  console.log(option)
+  if (option === 'start'){
+    routeChangeStart();
+  }
+  else if (option === 'join'){
+    routeChangeJoin();
+  }
+}
 
   return (
     <>
@@ -35,7 +75,12 @@ function StartOrJoinScreen(props) {
         <Modal.Body>Please enter room number</Modal.Body>
         <Modal.Footer>
         <form>
-              <input type="text" />
+        <input
+                  type="text"
+                  placeholder="Type Room Number "
+                  value={roomCode}
+                  onChange={(e) => setRoomCode(e.target.value)}
+        />
             <Button variant="primary" onClick={routeChange}>
             Enter
           </Button>
@@ -45,16 +90,16 @@ function StartOrJoinScreen(props) {
           </Button>
         </Modal.Footer>
       </Modal>
-
+      
       <StartJoinScreen>
 
         <h1>Selected Game: {selectedGame}</h1>
 
-        <GameboxStart onClick={handleShow}>
+        <GameboxStart onClick={() => {setOption("start"); setShow(true); }}>
           <h1>Start New Game</h1>
         </GameboxStart>
 
-        <GameboxJoin onClick={handleShow}>
+        <GameboxJoin onClick={() => {setOption("join"); setShow(true); }}>
           <h1>Join Game</h1>
         </GameboxJoin>
       </StartJoinScreen>
