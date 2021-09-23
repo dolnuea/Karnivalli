0c8ee57ffaea6dfa11da48d394e2938fd9a5fe69
Luna Dagci <ldagci@albany.edu>
2021-09-22 22:51:36 -0400

    Merge branch 'LunaBranch' into main

--- karnivali_UI/karnivaliui/src/webPages/ticTacToe.js
+++ karnivali_UI/karnivaliui/src/webPages/ticTacToe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header';
 import TicTacToeBody from './components/ticTacToeBody';
+import ScreenImg from  '../images/wrinkledpaper.jpg';
 
 const TicTacToe = (props) => {
     console.log("TicTacToe")
@@ -12,7 +13,7 @@ const TicTacToe = (props) => {
         playerColor = "blue"
     }
     return (
-        <div className="TicTacToe">
+        <div className="TicTacToe" style = {{backgroundImage : `url(${ScreenImg})` }}>
             <Header />
             <TicTacToeBody roomNumber={props.location.state.roomCode} playColor={playerColor} />
         </div>
