a3b900f3751ed06032d366949707f1bb55b6537c
Luna Dagci <ldagci@albany.edu>
2021-10-14 15:11:37 -0400

    Added minesweeper file

---
+++ karnivali_UI/karnivaliui/src/webPages/components/minesweeper.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect} from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { Modal, Button } from "react-bootstrap";
+import { useHistory } from 'react-router-dom';
