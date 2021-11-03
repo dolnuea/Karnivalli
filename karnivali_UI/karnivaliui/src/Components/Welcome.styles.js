93826f6abe8fd50fef0676da185e35740ac730fc
Qian Wang <qwang2@albany.edu>
2021-11-03 22:31:42 +0000

    Replace Welcome.styles.js

--- karnivali_UI/karnivaliui/src/Components/Welcome.styles.js
+++ karnivali_UI/karnivaliui/src/Components/Welcome.styles.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 export const StartButton = styled.button`
     height: 40px;
     width: 200px;
-    background-color: rgb(149, 112, 221);
+    background-color: rgb(129, 221, 90);
     color: rgb(88, 24, 69, 0.7);
     font-weight: 900;
     padding: 5px 10px 5px 10px;
@@ -16,13 +16,13 @@ export const StartButton = styled.button`
 export const WelcomeWindow = styled.div `
     height: 240px;
     width: 460px;
-    background-color: rgb(124, 64, 169);
+    background-color: rgb(255, 230, 239, 0.7);
     color: rgb(255, 195, 15);
-    border-top-right-radius: 50px;
-    border-bottom-left-radius: 50px;
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0px;
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 25vh;
-    border: rgb(88, 24, 69, 0.7)  10px solid;
+    
 `
