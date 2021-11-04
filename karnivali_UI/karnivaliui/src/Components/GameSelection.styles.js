731ee58d37c28b4f20513668e0a15dff44d33bdb
Qian Wang <qwang2@albany.edu>
2021-11-04 02:15:21 +0000

    Replace GameSelection.styles.js

--- karnivali_UI/karnivaliui/src/Components/GameSelection.styles.js
+++ karnivali_UI/karnivaliui/src/Components/GameSelection.styles.js
@@ -11,15 +11,14 @@ export const Panel = styled.div `
     height: 50%;
     padding-left: 20px;
     padding-right: 20px;
-    border: 10px solid;
-    border-radius: 12%;
-    border-color:  rgb(88, 24, 69, 0.7);
+    
+    
     display: flex;
     flex-flow: column;
     justify-content: center;
     align-items: center;
     color: rgb(255, 195, 15, 0.7);
-    background-color: rgb(149, 112, 221);
+    background-color: rgb(255, 230, 239, 0.6);
     transition-property: transform, background-color, color;
     transition-duration: .3s;
     transition-timing-function: cubic-bezier(0.37, 0, 0.63, 1);
