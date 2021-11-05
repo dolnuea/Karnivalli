0c0e9317712f52c44871a278c8ac52be08b32235
Qian Wang <qwang2@albany.edu>
2021-11-05 18:54:22 +0000

    Update ticTacToe.js background

--- karnivali_UI/karnivaliui/src/webPages/ticTacToe.js
+++ karnivali_UI/karnivaliui/src/webPages/ticTacToe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/header';
 import TicTacToeBody from './components/ticTacToeBody';
-import ScreenImg from  '../images/Tic_Tac_Toe1.jpg';
+import ScreenImg from  '../images/ttt.jpg';
 
 
 const TicTacToe = (props) => {
