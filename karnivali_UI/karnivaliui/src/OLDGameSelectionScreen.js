9c53337d9702481fba5ddcf13a20fc7fd99e9e42
Jacob <clouse.jp@gmail.com>
2021-11-15 17:45:17 -0500

    added minesweeper button to the game selection screen, have to change url so it directs to minesweeper instead of tic tac toe

--- karnivali_UI/karnivaliui/src/OLDGameSelectionScreen.js
+++ karnivali_UI/karnivaliui/src/OLDGameSelectionScreen.js
@@ -2,7 +2,6 @@ import { Panel, SelectionPanel } from "./Components/GameSelection.styles";
 import { useHistory } from 'react-router-dom';
 import ticTacToeImage from './images/tic-tac-toe.png';
 import RockPaperScissorsImage from './images/rock-paper-scissors.png';
-import mineSweeperImage from './images/minesweeper USE THIS ONE.svg';
 
 
 function GameSelectionScreen() {
@@ -20,19 +19,13 @@ function GameSelectionScreen() {
 
     return (
         <SelectionPanel>
-            <Panel hoverColor='#DAF7A6' onClick={() => routeChange('tic-tac-toe')}>
-                <img src={ticTacToeImage} height='60%'></img>
+            <Panel hoverColor='#2E64FE' onClick={() => routeChange('tic-tac-toe')}>
+                <img src={ticTacToeImage} height='80%'></img>
                 <h1>TicTacToe</h1>
             </Panel>
-            <Panel hoverColor='#FF5733' onClick={() => routeChange('rock-paper-scissor')}>
-                <img src={RockPaperScissorsImage} height='60%'></img>
+            <Panel hoverColor='coral' onClick={() => routeChange('rock-paper-scissor')}>
+                <img src={RockPaperScissorsImage} height='80%'></img>
                 <h1>Rock Paper Scissors</h1>
-            </Panel>
-
-            <Panel hoverColor='#FFC300' onClick={() => routeChange('rock-paper-scissor')}>
-                <img src={mineSweeperImage} height='60%'></img>
-                <h1>4D Minesweeper</h1>
-                
             </Panel>
         </SelectionPanel>
     );
