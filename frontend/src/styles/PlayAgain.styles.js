a53dadf9b8ff48859a871ff5dcbb4d6bf7325ec9
jc366512 <jclouse@albany.edu>
2021-11-24 22:59:30 -0500

    added animations to the play again screen, changed background so it is in line with the rest of the site, added pictures to both icons,icons, added animations (rotation to play again svg, fade in and out for game select screen) to both the icons and the panels themselves. All that is lefis left is to work on tic tac toe, deploy, and bugtest with selenium

--- frontend/src/styles/PlayAgain.styles.js
+++ frontend/src/styles/PlayAgain.styles.js
@@ -10,35 +10,94 @@ export const PlayAgainScreen = styled.div `
 
 export const PlayAgainWinLose = styled.div `
     display: flex;
-    color: whitesmoke;
-    border: none;
-    height: 500px;
-    width: 600px;
-    background-color: #693699;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    color: rgb(255, 195, 15, 0.7);
+    background-color: rgb(255, 230, 239, 0.6);
+    height: 50%;
+    // padding-left: 20px;
+    // padding-right: 20px;
+    min-width: 500px;
 
-    transition-property: transform, background-color, color;
+    transition-property: background-color, color, border-radius;
     transition-duration: .3s;
-    transition-timing-function: cubic-bezier(0.37, 0, 0.63, 1);
+
 
     &:hover{
         background-color: #ff124f;
+
+        animation: pulse 2s;
+        animation-iteration-count: infinite;
+
+        @keyframes pulse {
+            0% {border-radius: 0em;}
+            50% {border-radius: 3em;}
+            100% {border-radius: 0em;}
+        }
     }
 `;
 
 
 export const ReplayGameBox = styled.div `
     display: flex;
-    color: whitesmoke;
-    border: none;
-    height: 300px;
-    width: 300px;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    color: rgb(255, 195, 15, 0.7);
+    background-color: rgb(255, 230, 239, 0.6);
+    height: 50%;
+    // padding-left: 20px;
+    // padding-right: 20px;
+    min-width: 500px;
 
-    background-color: #9570DD;
-    transition-property: transform, background-color, color;
+
+    transition-property: background-color, color, border-radius;
     transition-duration: .3s;
-    transition-timing-function: cubic-bezier(0.37, 0, 0.63, 1);
+
 
     &:hover{
         background-color: #fe75fe;
+
+
+        animation: pulse 2s;
+        animation-iteration-count: infinite;
+
+        @keyframes pulse {
+            0% {border-radius: 0em; }
+            50% {border-radius: 3em; }
+            100% {border-radius: 0em; }
+        }
     }
+`;
+
+export const ReplayIconSVG = styled.img `
+
+    &:hover{
+        animation: rotation 2s infinite linear;
+
+        @keyframes rotation {
+                from {
+                        transform: rotate(0deg);
+                }
+                to {
+                        transform: rotate(-359deg);
+                }
+        }
+        // animation inspired from: https://codepen.io/vitoralberto/pen/OPYyYB
+    }
+`;
+
+export const GameSelectIconSVG = styled.img `
+    &:hover{
+        animation: fadeInAndOut 3s infinite linear;
+
+        @keyframes fadeInAndOut {
+            0% {opacity: 1;}
+            50% {opacity: 0.5;}
+            100% {opacity: 1;}
+        }
+
+    }
+
 `;
