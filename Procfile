046ca95b191a0943633046b5bf82eaffb663afbc
jc366512 <jclouse@albany.edu>
2021-12-01 09:46:23 -0500

    editing heroku config for deployment (ie: changing requirements, adding daphne, editing asgi

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn karnivali.wsgi
+web: daphne chat.asgi:application --port $PORT --bind 0.0.0.0 -v2
+chatworker: python manage.py runworker --settings=chat.settings -v2
--- backend/karnivali/asgi.py
+++ backend/karnivali/asgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
+from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -17,7 +19,7 @@ from .consumers_rps import RPS
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'karnivali.settings')
-
+django.setup()
 application = get_asgi_application()
 
 
